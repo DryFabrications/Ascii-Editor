ff97824107259120aa6a5cc8b2c96859ac4576e6
Jeremy Kuhlmann <jkuhlmann0782@gmail.com>
2019-01-21 22:41:04 -0800

    add settings.py and basic config.json

---
+++ settings.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+INSTALL_DIR = os.path.dirname(os.path.realpath(__file__))
+CONFIG_FILE = os.path.join(INSTALL_DIR, 'config.json')
+
+
+def read_config(config_file):
+    """Reads the config json file and returns the contents as a dictionary"""
+    with open(config_file) as config_data:
+        return json.load(config_data)
+
+
+def resolution(config):
+    """Returns the x and y values for the resolution of your canvas"""
+    res = config['resolution']
+    return res.split(',')
